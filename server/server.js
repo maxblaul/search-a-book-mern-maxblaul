0a8c974dfcaa6eb0767e9cf4d06de36ee44a1c40
maxblaul <144166736+maxblaul@users.noreply.github.com>
2023-12-03 18:56:43 -0600

    fix server.js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,56 @@
+// const express = require('express');
+// const path = require('path');
+// const db = require('./config/connection');
+// const routes = require('./routes');
+// // Utilize Apollo server and apply to Express server
+// const { ApolloServer } = require('@apollo/server');
+// const { expressMiddleware } = require('@apollo/server/express4');
+// const { authMiddleware } = require('./utils/auth');
+
+// const { typeDefs, resolvers } = require('./schemas');
+
+// const app = express();
+// const PORT = process.env.PORT || 3001;
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+
+// const startApolloServer = async () => {
+//   await server.start();
+
+//   app.use(express.urlencoded({ extended: true }));
+//   app.use(express.json());
+
+//   // This line should be after body parsing middleware
+//   app.use('/graphql', expressMiddleware(server));
+
+//   // if we're in production, serve client/build as static assets
+//   if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../client/build')));
+//   }
+
+//   app.use(routes);
+
+//   db.once('open', () => {
+//     app.listen(PORT, () =>
+//       console.log(`🌍 Now listening on localhost:${PORT}`)
+//     );
+//     console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+//   });
+// };
+
+// startApolloServer();
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
-// Utilize Apollo server and apply to Express server
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
-const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -18,25 +58,26 @@ const server = new ApolloServer({
 
 const startApolloServer = async () => {
   await server.start();
-
+  
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
-
-  // This line should be after body parsing middleware
+  
   app.use('/graphql', expressMiddleware(server));
 
-  // if we're in production, serve client/build as static assets
+  // if we're in production, serve client/dist as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-  }
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.use(routes);
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  } 
 
   db.once('open', () => {
-    app.listen(PORT, () =>
-      console.log(`🌍 Now listening on localhost:${PORT}`)
-    );
-    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    });
   });
 };
 
